978c5b2d583e90140b0709bc079fe67f15d7d12f
FaliIrham <falimagemaulana64@gmail.com>
2025-05-15 12:01:21 +0700

    migrations

---
+++ database/migrations/2025_05_15_044055_create_m_feedback_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_feedback', function (Blueprint $table) {
+            $table->id('id_feedback');
+            $table->unsignedBigInteger('id_pengguna');
+            $table->unsignedBigInteger('id_laporan');
+            $table->integer('penilaian');
+            $table->text('komentar')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->foreign('id_pengguna')
+                  ->references('id_pengguna')
+                  ->on('m_user')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->foreign('id_laporan')
+                  ->references('id_laporan')
+                  ->on('m_laporan_perbaikan')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_feedback');
+    }
+};
---
+++ database/migrations/2025_05_15_045026_create_m_riwayat_perbaikan_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_riwayat_perbaikan', function (Blueprint $table) {
+             $table->id('id_riwayat');
+            $table->unsignedBigInteger('id_laporan');
+            $table->text('tindakan_dilakukan');
+            $table->text('material_dipakai')->nullable();
+            $table->decimal('biaya', 10, 2)->nullable();
+            $table->unsignedBigInteger('diperbaiki_oleh')->nullable();
+            $table->timestamp('tanggal_perbaikan')->useCurrent();
+            $table->foreign('id_laporan')
+                  ->references('id_laporan')
+                  ->on('m_laporan_perbaikan')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->foreign('diperbaiki_oleh')
+                  ->references('id_pengguna')
+                  ->on('m_user')
+                  ->OnDelete()
+                  ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_riwayat_perbaikan');
+    }
+};
