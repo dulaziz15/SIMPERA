2dd5606ec3aa20cac36af95bdd3f9b67ec0f62a4
ABDUL AZIZ <107160974+dulaziz15@users.noreply.github.com>
2025-05-20 00:02:03 +0700

    manage menu & add login page

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,11 +4,11 @@
             <div class="col-sm-6">
                 <script>
                     document.write(new Date().getFullYear())
-                </script> © Minia.
+                </script> © SIMPERA.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by <a href="#!" class="text-decoration-underline">Themesbrand</a>
+                    Design & Develop by <a href="" class="text-decoration-underline">SIMPERA</a>
                 </div>
             </div>
         </div>
