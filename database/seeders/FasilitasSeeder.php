b54cd2254d3266fe7616c8c145c9024e4c56c1f1
FaliIrham <falimagemaulana64@gmail.com>
2025-05-23 04:12:46 +0700

    add ruangan & dukungan

--- database/seeders/FasilitasSeeder.php
+++ database/seeders/FasilitasSeeder.php
@@ -10,44 +10,31 @@ class FasilitasSeeder extends Seeder
 {
     public function run(): void
     {
-        $kategoriList = DB::table('m_kategori_fasilitas')->pluck('id_kategori', 'kode');
-
-        $gedungList = DB::table('m_gedung')->pluck('id_gedung', 'kode');
-
-        $fasilitas = [
+        DB::table('m_fasilitas')->insert([
             [
-                'nama' => 'Proyektor Ruang 101',
-                'kode_kategori' => 'FSI',
-                'kode_gedung' => 'AB',
-                'lokasi' => 'Ruang 101',
+                'nama' => 'Proyektor Epson',
+                'id_kategori' => 2,
+                'id_ruangan' => 1,
                 'status' => 'berfungsi',
-            ],
-            [
-                'nama' => 'AC Ruang 202',
-                'kode_kategori' => 'FSP',
-                'kode_gedung' => 'AB',
-                'lokasi' => 'Ruang 202',
-                'status' => 'berfungsi',
-            ],
-            [
-                'nama' => 'Sofa Tamu',
-                'kode_kategori' => 'FSU',
-                'kode_gedung' => 'AA',
-                'lokasi' => 'Ruang AA03',
-                'status' => 'berfungsi',
-            ],
-        ];
-
-        foreach ($fasilitas as $item) {
-            DB::table('m_fasilitas')->insert([
-                'nama' => $item['nama'],
-                'id_kategori' => $kategoriList[$item['kode_kategori']] ?? null,
-                'id_gedung' => $gedungList[$item['kode_gedung']] ?? null,
-                'lokasi' => $item['lokasi'],
-                'status' => $item['status'],
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
-            ]);
-        }
+            ],
+            [
+                'nama' => 'AC Panasonic',
+                'id_kategori' => 2,
+                'id_ruangan' => 1,
+                'status' => 'berfungsi',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'nama' => 'Meja Kayu',
+                'id_kategori' => 3,
+                'id_ruangan' => 2,
+                'status' => 'rusak ringan',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
     }
 }
