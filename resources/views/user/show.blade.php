10aad440440c0d3d0608ecd79706d5b3c5fed589
mabdullatifm1127 <mabdullatifm1127@gmail.com>
2025-05-24 22:43:53 +0700

    Show User

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,28 +1,16 @@
-{{-- @if(empty($profil->pengguna)) 
-    
-@include('user.profilNotFound')
 
-<<<<<<< HEAD
     <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Data User</h5>
+                <h5 class="modal-title">Show Data User</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Data Profile</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{{ $user->nama_pengguna }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+            <div class="modal-body row g-3">
+                {{ $user->nama_pengguna }}
+            </div>
             <div class="modal-footer">
                 
                 <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Batal</button>
+            </div>
+        </div>
+    </div>
