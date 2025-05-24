8193acee4d057fb2e50bc4faf6a21c78c12ede44
mabdullatifm1127 <mabdullatifm1127@gmail.com>
2025-05-24 22:20:32 +0700

    Show User

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -2,12 +2,23 @@
     <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Show Data User</h5>
+                <h5 class="modal-title">Data User</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body row g-3">
-                {{ $user->nama_pengguna }}
-            </div>
+            <div class="modal-body">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Data User</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ $user->nama_pengguna }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
             <div class="modal-footer">
                 
                 <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Batal</button>
