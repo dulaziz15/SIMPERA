28f868f01e4adfaeae419f9fb42c4def3c53883d
mabdullatifm1127 <mabdullatifm1127@gmail.com>
2025-05-24 22:09:43 +0700

    Show Kategori & Gedung

--- resources/views/kategoriGedung/show.blade.php
+++ resources/views/kategoriGedung/show.blade.php
@@ -1,7 +1,7 @@
 <div id="modal-master" class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title">Show Data Kategori Gedung</h5>
+            <h5 class="modal-title">Data Kategori Gedung</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
 
