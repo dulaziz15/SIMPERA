eb2917b6ebf0a78f86d22d8e0eebe84cb15f523d
mabdullatifm1127 <mabdullatifm1127@gmail.com>
2025-05-26 21:04:44 +0700

    index

--- resources/views/laporanPeriode/index.blade.php
+++ resources/views/laporanPeriode/index.blade.php
@@ -14,5 +14,50 @@
                             </div>
                             <!-- end col -->
 
-                            
+                             <div class="row">
+                        <div class="col-xl-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">Table head</h4>
+                                    <p class="card-title-desc">Use one of two modifier classes to make
+                                        <code>&lt;thead&gt;</code>s appear light or dark gray.</p>
+                                </div>
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table mb-0">
+
+                                            <thead class="table-light">
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>First Name</th>
+                                                    <th>Last Name</th>
+                                                    <th>Username</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row">1</th>
+                                                    <td>Mark</td>
+                                                    <td>Otto</td>
+                                                    <td>@mdo</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">2</th>
+                                                    <td>Jacob</td>
+                                                    <td>Thornton</td>
+                                                    <td>@fat</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">3</th>
+                                                    <td>Larry</td>
+                                                    <td>the Bird</td>
+                                                    <td>@twitter</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <!-- end card body -->
+                            </div>
+                            <!-- end card -->
 @endsection
