9eedec4ab83439c70e807cb753f2a069205045a1
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2025-05-27 00:30:01 +0700

    Update Periode

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('index');
+});
+
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/proses_login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout']);
