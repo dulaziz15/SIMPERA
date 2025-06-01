b21960d7b3a1cc70bffbfe68933d4f6b148fde58
mabdullatifm1127 <mabdullatifm1127@gmail.com>
2025-06-01 20:39:27 +0700

    index laporan periode

--- resources/views/laporanPeriode/index.blade.php
+++ resources/views/laporanPeriode/index.blade.php
@@ -5,84 +5,66 @@
 <html lang="en">
 <head>
 
-    <div class="container mt-5">
-    <div class="row">
-        <div class="col-xl-6">
-            <h4 class="card-title">Pie Chart</h4>
-            <canvas id="devicePieChart" width="90" height="90"></canvas>
-            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-            <script>
-                const ctx = document.getElementById('devicePieChart').getContext('2d');
-                const data = {
-                    labels: ['Laptop', 'Tablet', 'Mobile', 'Others', 'Desktop'],
-                    datasets: [{
-                        data: [15, 10, 10, 5, 60],
-                        backgroundColor: [
-                            '#f44336',
-                            '#4caf50',
-                            '#2196f3',
-                            '#ffc107', 
-                            '#3f51b5' 
-                        ],
-                        borderWidth: 1
-                    }]
-                };
-                const config = {
-                    type: 'pie',
-                    data: data,
-                    options: {
-                        plugins: {
-                            legend: {
-                                position: 'right'
-                            }
-                        }
-                    }
-                };
-                new Chart(ctx, config);
-            </script>
-        </div>
-
-
-        <div class="col-xl-6">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">Table Rank</h4>
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table mb-0">
-                            <thead class="table-light">
-                                <tr>
-                                    <th>No</th>
-                                    <th>Nama Kategori</th>
-                                    <th>Rank</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="container mt-5">
+  <div class="row">
+    <div class="col-xl-6 d-flex justify-content-center align-items-center">
+      <div style="width: 320px; height: 320px;">
+        <h4 class="card-title">Pie Chart</h4>
+        <canvas id="devicePieChart"></canvas>
+      </div>
     </div>
+
+    <div class="col-xl-6">
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title">Table Rank</h4>
+        </div>
+        <div class="card-body">
+          <table class="table">
+            <thead class="table-light">
+              <tr>
+                <th>No</th>
+                <th>Nama Kategori</th>
+                <th>Rank</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr><th scope="row">1</th><td>Mark</td><td>Otto</td></tr>
+              <tr><th scope="row">2</th><td>Jacob</td><td>Thornton</td></tr>
+              <tr><th scope="row">3</th><td>Larry</td><td>the Bird</td></tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+  const ctx = document.getElementById('devicePieChart').getContext('2d');
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ['Laptop', 'Tablet', 'Mobile', 'Others', 'Desktop'],
+      datasets: [{
+        data: [15, 10, 10, 5, 60],
+        backgroundColor: [
+          '#f44336', '#4caf50', '#2196f3', '#ffc107', '#3f51b5'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          position: 'right'
+        }
+      }
+    }
+  });
+</script>
 
    <div class="row">
                             <div class="col-12">
